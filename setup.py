a6cc71deaa8ef9bd079039a58d0dd3e4892830c1
Joshua Arnott <josh@snorfalorpagus.net>
2016-02-10 21:59:42 +0000

    Moved C extension into 'hello' package.

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 ext_modules = [
-    Extension("hello", ["hello.pyx"])
+    Extension("hello._hello", ["hello/_hello.pyx"])
 ]
 
 setup(
-  name = 'Hello world app',
-  ext_modules = ext_modules,
+    name = "Hello world!",
+    version = 1.0,
+    ext_modules = ext_modules,
+    packages = ["hello"],
 )
